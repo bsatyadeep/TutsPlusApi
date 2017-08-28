3ffeea0137e11bfe3f35a07c25ba90117fb3e3ce
PF1V27G <PF1V27G@PF1V27G>
2017-08-28 21:02:09 +0530

    Completed Demo2

---
+++ TutsPlusApi.Demo2/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TutsPlusApi.Demo2.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public int IWantANumber()
+        {
+            return 1000;
+        }
+        [HttpGet]
+        public int IWantANumber(int id)
+        {
+            return id;
+        }
+    }
+}
