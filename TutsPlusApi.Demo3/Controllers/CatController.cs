5f37a1bae640c607943d0161cee9074293d81bba
PF1V27G <PF1V27G@PF1V27G>
2017-08-28 21:14:32 +0530

    Complete Demo3

---
+++ TutsPlusApi.Demo3/Controllers/CatController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TutsPlusApi.Demo3.Controllers
+{
+    public class CatController : ApiController
+    {
+        private static List<string> _listCats;
+
+        public CatController()
+        {
+            if (_listCats == null)
+            {
+                _listCats = new List<string>
+                {
+                    "John",
+                    "Paul",
+                    "Geroge"
+                };
+            }
+        }
+
+        public List<string> Get()
+        {
+            return _listCats;
+        }
+
+        public string Get(int id)
+        {
+            return _listCats.Count > id ? _listCats[id] : string.Empty;
+        }
+    }
+}
---
+++ TutsPlusApi.Demo3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TutsPlusApi.Demo3.WebApiApplication" Language="C#" %>
